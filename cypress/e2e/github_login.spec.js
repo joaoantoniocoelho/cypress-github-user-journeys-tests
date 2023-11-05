4af9fe812fc833a18f3b29673cd93c269bbb19d9
João Antonio Coelho <joaoantonioscoelho@gmail.com>
2023-11-05 16:23:00 -0300

    adição de testes com o cypress + script para executá-los

---
+++ cypress/e2e/github_login.spec.js
@@ -0,0 +1,33 @@
+describe('GitHub Repository Management Workflow', () => {
+    it('Logs in with valid credentials', () => {
+        cy.visit('https://github.com/login');
+
+        cy.get('input[name="login"]').type(Cypress.env('GITHUB_USERNAME'));
+        cy.get('input[name="password"]').type(Cypress.env('GITHUB_PASSWORD'));
+        cy.get('input[name="commit"]').click();
+
+        cy.get('body').should('have.class', 'logged-in');
+    });
+
+    it('Navigates to Create New Repository Page after login', () => {
+        cy.visit('https://github.com/login');
+
+        cy.get('input[name="login"]').type(Cypress.env('GITHUB_USERNAME'));
+        cy.get('input[name="password"]').type(Cypress.env('GITHUB_PASSWORD'));
+        cy.get('input[name="commit"]').click();
+        cy.get('.dashboard-sidebar').find('a[href="/new"]').should('be.visible').click();
+        cy.get('form h1').should('have.text', 'Create a new repository');
+    });
+
+    it('Edit profile must be visible on logged user profile', () => {
+        cy.visit('https://github.com/login');
+
+        cy.get('input[name="login"]').type(Cypress.env('GITHUB_USERNAME'));
+        cy.get('input[name="password"]').type(Cypress.env('GITHUB_PASSWORD'));
+        cy.get('input[name="commit"]').click();
+
+        cy.visit(`https://github.com/${Cypress.env('GITHUB_USERNAME')}`);
+        // ..js-profile-editable-area should have button containing text 'Edit profile'
+        cy.get('.js-profile-editable-area').find('button').contains('Edit profile').should('be.visible');
+    });
+});
--- index.js
+++ index.js
@@ -4,9 +4,12 @@ cypress.run({
     // Configuração para executar testes específicos no diretório cypress/e2e
     specPattern: "cypress/e2e/**/*.*",
 })
-    .then((results) => {
-        console.log("Cypress run completed with the following results:", results);
+    .then(() => {
+
     })
     .catch((err) => {
         console.error("An error occurred during the Cypress run:", err);
-    });
+    })
+    .finally(() => {
+        console.log("End of tests execution");
+    })
